fc88defd4177c7deab79bd058105bd8e34ada0e1
=Salman nazeer <salmanselu2002@gmail.com>
2021-09-19 06:10:26 +0530

    Automatic certificate generator

---
+++ main.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+
+cx = 843
+cy = 577
+df = pd.read_csv('list.csv')
+font = ImageFont.truetype('italianno.ttf',70)
+fill = (207,255,229) 
+for index,j in df.iterrows():
+    image = Image.open('template.png')
+    canvas = ImageDraw.Draw(image)
+    name = j['name']
+    email = j['email']
+    w, h = font.getsize(name)
+    canvas.text(xy = ( cx - w/2, cy), text=f'{name}', fill=fill, font=font)
+    image.save(f'pictures/{email}.png')
