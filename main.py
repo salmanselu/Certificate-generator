fd0d2360fbf041342c40017130bf47a6b732c1a5
=Salman nazeer <salmanselu2002@gmail.com>
2021-09-19 07:20:52 +0530

    Added some input checking

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
-
 cx = 843
 cy = 577
 df = pd.read_csv('list.csv')
 font = ImageFont.truetype('italianno.ttf',140)
 fill = (207,255,229) 
-for index,j in df.iterrows():
+for j in df.iterrows():
     image = Image.open('template.png')
     canvas = ImageDraw.Draw(image)
-    name = j['name']
+
+    person = j['name']
+    if person.isupper(): # if the person has entered their name
+        continue         # in full uppercase, we skip their 
+                         # name to manually process it later.
+
     email = j['email']
-    w, h = font.getsize(name)
-    canvas.text(xy = ( cx - w/2, cy - h/2), text=f'{name}', fill=fill, font=font)
+    w, h = font.getsize(person)
+    canvas.text(xy = ( cx - w/2, cy - h/2), text=f'{person}', fill=fill, font=font)
     image.save(f'generated-certificates/{email}.png')
