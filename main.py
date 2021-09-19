c0f2eef8ee7d3bc7abb8cdce22e96c1841488640
=Salman nazeer <salmanselu2002@gmail.com>
2021-09-19 06:51:52 +0530

    Corrected text cenetring

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 cx = 843
 cy = 577
 df = pd.read_csv('list.csv')
-font = ImageFont.truetype('italianno.ttf',70)
+font = ImageFont.truetype('italianno.ttf',140)
 fill = (207,255,229) 
 for index,j in df.iterrows():
     image = Image.open('template.png')
@@ -12,5 +12,5 @@ for index,j in df.iterrows():
     name = j['name']
     email = j['email']
     w, h = font.getsize(name)
-    canvas.text(xy = ( cx - w/2, cy), text=f'{name}', fill=fill, font=font)
-    image.save(f'pictures/{email}.png')
+    canvas.text(xy = ( cx - w/2, cy - h/2), text=f'{name}', fill=fill, font=font)
+    image.save(f'generated-certificates/{email}.png')
